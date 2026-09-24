fc54c56f689ef6fd483bdb793763339fa1d104ac
Immanuel <emmanuelmendoza448@gmail.com>
2026-09-24 15:07:38 -0600

    Implementar consulta de productos activos

---
+++ InventoryApi/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using InventoryApi.DTOs;
+
+namespace InventoryApi.Services;
+
+public interface IProductService
+{
+    Task<List<ProductDto>> GetAllAsync();
+
+    Task<ProductDto?> GetByIdAsync(int id);
+
+    Task<ProductDto> CreateAsync(CreateProductDto dto);
+
+    Task<bool> UpdateAsync(int id, UpdateProductDto dto);
+
+    Task<bool> DeleteAsync(int id);
+}
---
+++ InventoryApi/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using InventoryApi.Data;
+using InventoryApi.DTOs;
+using InventoryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApi.Services;
+
+public class ProductService : IProductService
+{
+    // guarda nuestro acceso al EF Core.
+    private readonly AppDbContext _context;
+
+    // es inyección de dependencias.
+    public ProductService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<ProductDto>> GetAllAsync()
+    {
+        //convierte cada Product de la base de datos en un ProductDto.
+        return await _context.Products
+            .Where(p => p.IsActive)
+            .Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                Stock = p.Stock,
+                IsActive = p.IsActive,
+                CreatedAt = p.CreatedAt,
+                UpdatedAt = p.UpdatedAt
+            })
+            .ToListAsync();
+    }
+
+    public Task<ProductDto?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDto> CreateAsync(CreateProductDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(int id, UpdateProductDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
