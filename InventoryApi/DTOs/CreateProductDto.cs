4acfb52da4375d2c6fce22c4694f51758cb5d170
Immanuel <emmanuelmendoza448@gmail.com>
2026-09-24 14:58:32 -0600

    Agregar validaciones de productos

--- InventoryApi/DTOs/CreateProductDto.cs
+++ InventoryApi/DTOs/CreateProductDto.cs
@@ -1,6 +1,19 @@
-﻿namespace InventoryApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryApi.DTOs;
+
+public class CreateProductDto
 {
-    public class CreateProductDto
-    {
-    }
+    [Required]
+    [MaxLength(150)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Description { get; set; }
+
+    [Range(0.01, double.MaxValue)]
+    public decimal Price { get; set; }
+
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
 }
